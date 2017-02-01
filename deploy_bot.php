3e73f0e12706fd7bc1795edad30f435a9f6e9d55
Namwoo Kim <iam@kimtree.net>
2017-02-01 12:31:53 +0900

    Migrate previous deploy to newly created branch

---
+++ deploy_bot.php
@@ -0,0 +1,36 @@
+<?php
+define("WEBROOT_PATH", "/var/www/html");
+define("BRANCH_TO_FOLLOW", "refs/heads/master");
+define("DEFAULT_TIMEZONE", "Asia/Seoul");
+
+date_default_timezone_set(DEFAULT_TIMEZONE);
+
+$payload = json_decode($_POST["payload"]);
+$log = '';
+
+if ($payload !== NULL && $payload->ref === BRANCH_TO_FOLLOW) {
+    // Modify UTC time to local time
+    $dt = new DateTime($payload->head_commit->timestamp);
+    $dt->setTimezone(new DateTimeZone(DEFAULT_TIMEZONE));
+
+    $log .= $dt->format('Y-m-d H:i:s') . ' ' . DEFAULT_TIMEZONE . PHP_EOL;
+    $log .= "Last Commit: " . $payload->head_commit->id . PHP_EOL;
+    $log .= "Author: " . $payload->head_commit->author->name . PHP_EOL;
+    $log .= $payload->head_commit->message . PHP_EOL;
+
+    // Export environment variable
+    exec("export CI_ENV='production'");
+    // Execute git command to sync
+    $git_sync_command = 'cd ' . WEBROOT_PATH . ' && git reset HEAD --hard && git pull';
+    exec($git_sync_command, $result_data, $result_code);
+
+    // To check git command output and return value
+    if ((strpos($result_data[1], "Updating") === false && strpos($result_data[1], "Already up-to-date.") === false) || $result_code !== 0) {
+        $log .= PHP_EOL . "Somethings Wrong";
+        // TODO: What to do when error occurs
+    } else {
+        $log .= PHP_EOL . "Successfully Updated";
+    }
+
+    echo $log;
+}
